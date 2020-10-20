8778c88d7f810db5043dc3bfa924112402cf6649
data-jay <72634251+data-jay@users.noreply.github.com>
2020-10-20 07:03:05 -0700

    Update create_table.py

--- create_table.py
+++ create_table.py
@@ -9,13 +9,13 @@ cursor.execute('''CREATE TABLE SCHOOL
          
 conn = sqlite3.connect('my_database.sqlite')
 cursor = conn.cursor()cursor.execute("INSERT INTO SCHOOL (ID,NAME,AGE,ADDRESS,MARKS) \
-      VALUES (1, 'Rohan', 14, 'Delhi', 200)");
+      VALUES (1, 'Jack', 14, 'San Jose', 200)");
 cursor.execute("INSERT INTO SCHOOL (ID,NAME,AGE,ADDRESS,MARKS) \
-      VALUES (2, 'Allen', 14, 'Bangalore', 150 )");
+      VALUES (2, 'Allen', 14, 'Campbell', 150 )");
 cursor.execute("INSERT INTO SCHOOL (ID,NAME,AGE,ADDRESS,MARKS) \
-      VALUES (3, 'Martha', 15, 'Hyderabad', 200 )");
+      VALUES (3, 'Martha', 15, 'Menlo', 200 )");
 cursor.execute("INSERT INTO SCHOOL (ID,NAME,AGE,ADDRESS,MARKS) \
-      VALUES (4, 'Palak', 15, 'Kolkata', 650)");
+      VALUES (4, 'Palak', 15, 'Fremont', 650)");
 
 conn.commit()
 conn.close()
