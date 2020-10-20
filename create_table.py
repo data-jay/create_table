436b686dfae185d54b094f24e21c7c9813d3ab05
data-jay <72634251+data-jay@users.noreply.github.com>
2020-10-20 07:00:29 -0700

    Update create_table.py

--- create_table.py
+++ create_table.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 cursor.execute('''CREATE TABLE SCHOOL
          (ID INT PRIMARY KEY     NOT NULL,
          NAME           TEXT    NOT NULL,
@@ -5,5 +7,16 @@ cursor.execute('''CREATE TABLE SCHOOL
          ADDRESS        CHAR(50),
          MARKS          INT);''')
          
-cursor.close()
+conn = sqlite3.connect('my_database.sqlite')
+cursor = conn.cursor()cursor.execute("INSERT INTO SCHOOL (ID,NAME,AGE,ADDRESS,MARKS) \
+      VALUES (1, 'Rohan', 14, 'Delhi', 200)");
+cursor.execute("INSERT INTO SCHOOL (ID,NAME,AGE,ADDRESS,MARKS) \
+      VALUES (2, 'Allen', 14, 'Bangalore', 150 )");
+cursor.execute("INSERT INTO SCHOOL (ID,NAME,AGE,ADDRESS,MARKS) \
+      VALUES (3, 'Martha', 15, 'Hyderabad', 200 )");
+cursor.execute("INSERT INTO SCHOOL (ID,NAME,AGE,ADDRESS,MARKS) \
+      VALUES (4, 'Palak', 15, 'Kolkata', 650)");
+
+conn.commit()
+conn.close()
          
